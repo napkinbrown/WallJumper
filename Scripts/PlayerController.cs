04aedb5eaf85e06eb2cfc4f2c744e61b6223cbd6
Dominic Brown <domingomonk@gmail.com>
2016-02-05 00:10:45 -0500

    Started on platform placer script. Need to update it so that there is a cursor instead of a platform, and clicking leaves a platform in that position
    Signed-off-by: Dominic Brown <domingomonk@gmail.com>

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,32 +6,44 @@ public class PlayerController : MonoBehaviour {
 	public Rigidbody2D rb;
 
 	public float runSpeed;
-	//public float maxSpeed;
 	public float jumpSpeed;
+	
+	public float platformDistanceX;
+	public float platformDistanceY;	
+	public GameObject platform;
 	
 	private bool canJump = true;
 	
 	void Start () 
 	{
 		rb.GetComponent<Rigidbody2D> ();
+		platform.GetComponent<GameObject>();
+		
+		Instantiate(platform,new Vector2(transform.position.x + platformDistanceX, transform.position.y + platformDistanceY),Quaternion.identity); //sets up platform
 	}
 
 	void FixedUpdate()
 	{
-		float running = Input.GetAxis ("Horizontal") * runSpeed * Time.deltaTime;
+		float running = Input.GetAxis ("Horizontal") * runSpeed * Time.deltaTime; //Horizontal movement
 		rb.AddForce(transform.right * running, ForceMode2D.Force);
 		
-		if (Input.GetButtonDown ("Jump") && canJump == true) 
+		if (Input.GetButtonDown ("Jump") && canJump == true) //Jumps if able to
 		{
 			rb.AddForce (transform.up * jumpSpeed, ForceMode2D.Force);
 			canJump = false;
 		}
 		
+		PlatformUpdate();
 	}
-	void OnCollisionEnter2D (Collision2D other)
+	void OnCollisionEnter2D (Collision2D other) //Resets jump
 	{
 		if (other.collider.CompareTag("Boundary"))
 			canJump = true;
+	}
+	private void PlatformUpdate() //Takes the position of the mouse and sets the player platform to that position but on a set radius
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Finds where 
+		//Debug.Log(ray);
 	}
 
 
