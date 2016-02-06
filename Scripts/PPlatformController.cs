e01a2acad6734385a17d8402c0f66a7f1696af57
Dominic Brown <domingomonk@gmail.com>
2016-02-06 01:04:00 -0500

    Platform now rotates around player and faces them at all times Need to add a function that spawns a platform on left click
    Signed-off-by: Dominic Brown <domingomonk@gmail.com>

--- Scripts/PPlatformController.cs
+++ Scripts/PPlatformController.cs
@@ -2,13 +2,14 @@
 using System.Collections;
 
 public class PPlatformController : MonoBehaviour {
-
+	
 	public float platformDistanceX;
 	public float platformDistanceY;	
 	public GameObject platform;
-
+	
 	private GameObject thisPlatform;
 	private Vector2 platformPosition;
+	private float rotationAngle;
 	
 	void Start () 
 	{
@@ -23,16 +24,22 @@ public class PPlatformController : MonoBehaviour {
 		Vector2 mousePosition = new Vector2(rayPoint.x,rayPoint.y);
 		
 		float angleToMouse = Mathf.Atan2(mousePosition.y,mousePosition.x); //Finds the angle between the character and the mouse
+		rotationAngle = angleToMouse;
 		
 		float xPlatformPos = platformDistanceX * Mathf.Cos (angleToMouse); 
 		float yPlatformPos = platformDistanceY * Mathf.Sin (angleToMouse);
-		Debug.Log(xPlatformPos);
-		Debug.Log(yPlatformPos);
+		//Debug.Log(xPlatformPos);
+		//Debug.Log(yPlatformPos);
 		
-		platformPosition = new Vector2(xPlatformPos,yPlatformPos);
+		platformPosition = new Vector2(xPlatformPos,yPlatformPos); // sets the platform where it should go rotation wise
 	}
 	void FixedUpdate()
 	{
-		thisPlatform.transform.position = platformPosition;
+		Vector2 playerTransform2D = new Vector2(transform.position.x,transform.position.y); //converts transform.postion to Vector2
+		thisPlatform.transform.position = platformPosition + playerTransform2D;
+		
+		float platformRotation = rotationAngle * 180 / Mathf.PI; //Rotates the platform so that a face is always facing the player
+		Vector3 rotation = new Vector3(0.0f,0.0f,platformRotation);
+		thisPlatform.transform.rotation = Quaternion.Euler(rotation);
 	}
 }
