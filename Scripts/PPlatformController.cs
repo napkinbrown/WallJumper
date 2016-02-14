a149af055df2cf13f9ad1ba78360782f945f9499
Dominic Brown <domingomonk@gmail.com>
2016-02-14 01:14:30 -0500

    Vector.2 angle is giving me the wrong numbers. I need to find the angle between the player and the mouse relative to the center of the screen.
    Signed-off-by: Dominic Brown <domingomonk@gmail.com>

--- Scripts/PPlatformController.cs
+++ Scripts/PPlatformController.cs
@@ -3,34 +3,30 @@ using System.Collections;
 
 public class PPlatformController : MonoBehaviour {
 	
-	public float platformDistanceX;
-	public float platformDistanceY;	
+	public float radius;
 	public GameObject platform;
 	public GameObject placedPlatform;
 	
 	private GameObject thisPlatform;
-	private Vector2 platformPosition;
 	private bool canPlacePlatform = true; //true only for testing purposes
 
 	
 	void Start () 
 	{
+		Cursor.visible = false;
 		platform.GetComponent<GameObject>();
-		thisPlatform = (GameObject)Instantiate(platform,new Vector2(transform.position.x + platformDistanceX, transform.position.y + platformDistanceY),Quaternion.identity); //sets up platform
+		thisPlatform = (GameObject)Instantiate(platform,new Vector2(transform.position.x + radius, transform.position.y + radius),Quaternion.identity); //sets up platform
 	}
 	void FixedUpdate()
 	{	
-		Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-		Debug.Log(mousePosition);
-		float angleToMouse = Vector2.Angle(transform.position,mousePosition); //Finds the angle between the character and the mouse
-		//Debug.Log(angleToMouse);
-
-		float xPlatformPos = platformDistanceX * Mathf.Cos (angleToMouse); 
-		float yPlatformPos = platformDistanceY * Mathf.Sin (angleToMouse);
-		//Debug.Log(xPlatformPos);
-		//Debug.Log(yPlatformPos);
+		//Finds where the player and the mouse is on the screen
+		Vector2 mousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		Vector2 playerPosition = Camera.main.WorldToViewportPoint(transform.position);
+		float angleToMouse = Vector2.Angle(playerPosition,mousePosition); //Finds the angle between the character and the mouse
 		
-		platformPosition = new Vector2(xPlatformPos,yPlatformPos); // sets the platform where it should go rotation wise
+		Debug.Log(angleToMouse); //VECTOR2.ANGLE IS WRONG
+		
+		/* Vector2 platformPosition = new Vector2(xPlatformPos,yPlatformPos); // sets the platform where it should go rotation wise
 		Vector2 playerTransform2D = new Vector2(transform.position.x,transform.position.y); //converts transform.postion to Vector2
 		thisPlatform.transform.position = platformPosition + playerTransform2D;
 		
@@ -41,6 +37,6 @@ public class PPlatformController : MonoBehaviour {
 		if ((canPlacePlatform) && (Input.GetMouseButtonDown(0)))
 		{
 			Instantiate(placedPlatform,thisPlatform.transform.position,thisPlatform.transform.rotation);
-		}
+		} */
 	}
 }
